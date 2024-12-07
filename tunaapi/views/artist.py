e5879bf9d50f614580bfaba073a7029d5c28861e
Brooks West <brooksmwest@hotmail.com>
2024-12-07 10:17:32 -0600

    got all of my get single items and lists going

---
+++ tunaapi/views/artist.py
@@ -0,0 +1,83 @@
+"""View module for handling requests about artists"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from tunaapi.models import Artist
+
+
+class ArtistView(ViewSet):
+    """Level up artist view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single artis type
+      
+        Returns:
+            Response -- JSON serialized artist
+        """
+
+        try:
+            artist = Artist.objects.get(pk=pk)
+            serializer = ArtistSerializer(artist)
+            return Response(serializer.data)
+        except Artist.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        """Handle GET requests to get all artists
+
+        Returns:
+            Response -- JSON serialized list of artists
+        """
+        artists = Artist.objects.all()
+
+        event_game = request.query_params.get('event', None)
+        if event_game is not None:
+            events = events.filter(event_id=event_game)
+
+        serializer = ArtistSerializer(artists, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+         """
+
+        artist = Artist.objects.create(
+            artist=artist,
+            name=request.data["name"],
+            age=request.data["age"],
+            bio=request.data["bio"],    
+        )
+        serializer = ArtistSerializer(artist)
+        return Response(serializer.data)
+    
+    def update(self, request, pk):
+        """Handle PUT requests for a game
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        artist = Artist.objects.get(pk=pk)
+        artist.name = request.data["name"]
+        artist.age = request.data["age"]
+        artist.bio = request.data["bio"]
+       
+        artist.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        event = Artist.objects.get(pk=pk)
+        event.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+class ArtistSerializer(serializers.ModelSerializer):
+    """JSON serializer for artists
+    """
+    class Meta:
+        model = Artist
+        depth = 1
+        fields = ('id', 'name', 'age', 'bio')
---
+++ tunaapi/views/song.py
@@ -0,0 +1,91 @@
+"""View module for handling requests about song genres"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from tunaapi.models import Song
+from tunaapi.models import Artist
+from tunaapi.models import SongGenre
+
+
+class SongView(ViewSet):
+    """Tuna Piano song view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single song
+
+        Returns:
+            Response -- JSON serialized game
+        """
+        song = Song.objects.get(pk=pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+
+
+    def list(self, request):
+        """Handle GET requests to get all songs
+
+        Returns:
+            Response -- JSON serialized list of songs
+        """
+        songs = Song.objects.all()
+        song_genre = request.query_params.get('genre', None)
+        if song_genre is not None:
+            songs = songs.filter(song_genre_id=song_genre)
+
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+             Response -- JSON serialized song instance
+        """
+        song = Song.objects.get(uid=request.data["song"])
+        song_genre = SongGenre.objects.get(pk=request.data["song_genre"])
+
+        song = Song.objects.create(
+            title=request.data["title"],
+            artist=artist,
+            album=request.data["album"],
+            length=request.data["length"],
+            song_genre=song_genre,
+            gamer=gamer
+        )
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    
+    def update(self, request, pk):
+        """Handle PUT requests for a game
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        song = Song.objects.get(pk=pk)
+        song.title = request.data["title"]
+        song.artist = request.data["artist_id"]
+        song.album = request.data["album"]
+        song.length = request.data["length"]
+
+        song_genre = SongGenre.objects.get(pk=request.data["song_genre"])
+        song.song_genre = song_genre
+        song.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        game = Song.objects.get(pk=pk)
+        game.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+        
+
+
+class SongSerializer(serializers.ModelSerializer):
+    """JSON serializer for song genres
+    """
+    class Meta:
+        model = Song
+        depth =1
+        fields = ('id', 'title', 'artist_id', 'album', 'length')
